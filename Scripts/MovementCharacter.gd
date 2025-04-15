2c57b037d82346bf0f13eb106d908603983ce1ef
Antonio Ramos Torres <antonioramostorres@gmail.com>
2025-04-15 20:04:36 +0200

    Add jumping counter and count jumps

--- Scripts/MovementCharacter.gd
+++ Scripts/MovementCharacter.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 var speed := 200
 var jump_force := -400
 var gravity := 900
+var jump_counter := 0
+
+@onready var jump_label = get_node("/root/Main/CanvasLayer/Counter")
 
 func _physics_process(delta):
 	var direction := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -18,9 +21,14 @@ func _physics_process(delta):
 		# Salto solo si está en el suelo
 		if Input.is_action_just_pressed("ui_accept"):  # Por defecto, barra espaciadora
 			velocity.y = jump_force
+			jump_counter += 1
+			update_jump_counter()
 
 	# Flip del sprite
 	if direction != 0:
 		$Sprite2D.flip_h = direction > 0
 
 	move_and_slide()
+	
+func update_jump_counter():
+	jump_label.text = "Saltos: %d" % jump_counter
