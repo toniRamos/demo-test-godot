d23e825682a6484e8c25a4f5d76d31adc10629fd
Antonio Ramos VIBIA <antonioramosvibia@Mac.home>
2025-04-16 14:16:31 +0200

    Create punch

--- Scripts/MovementCharacter.gd
+++ Scripts/MovementCharacter.gd
@@ -5,8 +5,13 @@ var speed := 200
 var jump_force := -400
 var gravity := 900
 var jump_counter := 0
+var is_attacking: bool = false
 
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_label = get_node("/root/Main/CanvasLayer/Counter")
+@onready var bate = get_node("/root/Main/Player/Bate")
+@onready var label_PAM = get_node("/root/Main/Player/Bate/Label")
+
 
 func _physics_process(delta):
 	var direction := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -14,12 +19,18 @@ func _physics_process(delta):
 	# Movimiento horizontal
 	velocity.x = direction * speed
 
+	if Input.is_action_just_pressed("ui_accept"):
+		is_attacking = true
+		anim_sprite.play("attack")
+		bate.disabled = false
+		label_PAM.text = "PAM!"
+
 	# Aplicar gravedad
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		# Salto solo si está en el suelo
-		if Input.is_action_just_pressed("ui_accept"):  # Por defecto, barra espaciadora
+		if Input.is_action_just_pressed("ui_up"):  # Por defecto, barra espaciadora
 			velocity.y = jump_force
 			jump_counter += 1
 			update_jump_counter()
@@ -32,3 +43,10 @@ func _physics_process(delta):
 	
 func update_jump_counter():
 	jump_label.text = "Saltos: %d" % jump_counter
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim_sprite.animation == "attack":
+		is_attacking = false
+		bate.disabled = true
+		label_PAM.text = ""
+		anim_sprite.play("stay")
